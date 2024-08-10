f6939ee49f49f92ef17eb2cadcc774f4b24c252f
sackie0813 <jsjsone0813@gmail.com>
2024-08-10 15:49:45 -0400

    Completed excersizes in README

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if(array.length === 0) {
+    return 0;
+  }
+	else {return array
+    .reduce((acc,curr) => acc+curr);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array
+    .reduce((acc,curr) => acc*curr);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let result = a;
+  for (let i = 1; i<b; i++) {
+    result += result*b;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+// 5! 
+/*
+  1*1 , 1*2, 2*3, 6*4, 24*5
+*/
+const factorial = function(a) {
+  let result = 1;
+  for (let i = 1; i < a; i++) {
+     result += result*i;
+  }
+	return result;
 };
 
 // Do not edit below this line
