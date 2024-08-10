1d204c21ca9e6f20bf714caa4d6fb7065fe76d51
sackie0813 <jsjsone0813@gmail.com>
2024-08-10 17:39:55 -0400

    completed excersize in README

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumeric = "abcdefghijklmnopqrstuvwxyz1234567890";
+    const newString = string
+        .toLowerCase()
+        .split("")
+        .filter((character) => alphanumeric.includes(character))
+        .join("");
 
+    const reversedString = newString
+        .split("")
+        .reverse()
+        .join("");
+
+    return newString === reversedString;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+/*
+racecar
+calico
+
+if (!string1.length == string2.length) return false;
+if ()
+
+
+*/
