05182674e09be257fb8cd883561104144ec922b7
sackie0813 <jsjsone0813@gmail.com>
2024-08-10 17:55:01 -0400

    Completed excersize in README

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    const fib = [0,1];
+    for (let i = 2; i<=num;i++){
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib[num];
 };
 
 // Do not edit below this line
