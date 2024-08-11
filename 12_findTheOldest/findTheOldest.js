e51ca02de61ae598b3ab9f26c224030d33c2fc8a
sackie0813 <jsjsone0813@gmail.com>
2024-08-11 00:46:21 -0400

    Completed Excersizes in README

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+function getAge (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
-};
+const findTheOldest = function(people) {
+    return people.reduce((oldest,currentPerson) =>  {
+        let currentAge = (getAge(currentPerson.yearOfBirth,currentPerson.yearOfDeath));
+        let oldestAge = (getAge(oldest.yearOfBirth, oldest.yearOfDeath));
+        if (currentAge > oldestAge) return currentPerson;
+        else return oldest;
+    });
+}
+
+/*
+
+*/
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
